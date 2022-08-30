03895d7bac1a2ac2df29b9299636b9ac940b549c
Cerize <mateusdc@gmail.com>
2022-08-30 16:54:37 -0300

    tests

--- src/test/java/com/cerize/reactive/test/FluxTest.java
+++ src/test/java/com/cerize/reactive/test/FluxTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -106,7 +108,7 @@ public class FluxTest {
             }
         });
 
-        StepVerifier.create(flux).expectNext()
+        StepVerifier.create(flux)
                 .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
                 .verifyComplete();
     }
@@ -136,9 +138,99 @@ public class FluxTest {
             }
         });
 
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberPrettyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+                .log()
+                .limitRate(3);
+
+        flux.subscribe(i -> log.info("Numeros: {}", i));
+
         StepVerifier.create(flux).expectNext()
                 .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
                 .verifyComplete();
     }
+
+
+    @Test
+    public void fluxSubscriberIntervalOne() throws InterruptedException {
+        Flux<Long> interval = Flux.interval(Duration.ofMillis(100))
+                .take(10)
+                .log();
+
+        interval.subscribe(i -> log.info("Number {}", i));
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void fluxSubscriberIntervalTwo() throws InterruptedException {
+        StepVerifier.withVirtualTime(this::createInterval)
+                .expectSubscription()
+                .expectNoEvent(Duration.ofDays(1))
+                .thenAwait(Duration.ofDays(1))
+                .expectNext(0L)
+                .thenAwait(Duration.ofDays(1))
+                .expectNext(1L)
+                .thenCancel()
+                .verify();
+    }
+
+    private Flux<Long> createInterval() {
+        return Flux.interval(Duration.ofDays(1))
+                .log();
+    }
+
+    @Test
+    public void connectableFlux() throws Exception {
+        ConnectableFlux<Integer> connectableFlux = Flux.range(1, 10)
+                .delayElements(Duration.ofMillis(100))
+                .log()
+                .publish();
+
+        connectableFlux.connect();
+
+//        log.info("Thread sleep for 300ms");
+//
+//        Thread.sleep(200);
+//
+//
+//        connectableFlux.subscribe(i -> log.info("Sub1 number {}", i));
+//
+//        log.info("Thread sleep for 200ms");
+//
+//        Thread.sleep(200);
+//
+//        connectableFlux.subscribe(i -> log.info("Sub 2 number {}", i));
+
+        StepVerifier.create(connectableFlux)
+                .then(connectableFlux::connect)
+                .thenConsumeWhile(i -> i <= 5)
+                .expectNext(6, 7, 8, 9, 10)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void connectableFluxAutoConnect() throws Exception {
+        Flux<Integer> fluxAutoConnect = Flux.range(1, 5)
+                .log()
+                //.delayElements(Duration.ofMillis(100))
+                .publish()
+                .autoConnect(3);
+
+        StepVerifier.create(fluxAutoConnect)
+                .then(fluxAutoConnect::subscribe)
+                .then(fluxAutoConnect::subscribe)
+                .expectNext(1,2,3,4,5)
+                .expectComplete()
+                .verify();
+    }
+
 
 }
---
+++ src/test/java/com/cerize/reactive/test/OperatorsTest.java
@@ -0,0 +1,129 @@
+package com.cerize.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class OperatorsTest {
+
+    @Test
+    public void subscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void publishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void multipleSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void multiplePublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void publishAndSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void subscribeAndPublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} and Thread {} ", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+}
