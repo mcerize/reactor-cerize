0f65d8a4c63c88d7f3306ec53db38e3ea6c1de4b
Cerize <mateusdc@gmail.com>
2022-08-31 16:46:19 -0300

    teste

--- src/test/java/com/cerize/reactive/test/OperatorsTest.java
+++ src/test/java/com/cerize/reactive/test/OperatorsTest.java
@@ -1,10 +1,16 @@
 package com.cerize.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Slf4j
 public class OperatorsTest {
@@ -125,5 +131,23 @@ public class OperatorsTest {
                 .expectSubscription()
                 .expectNext(1, 2, 3, 4)
                 .verifyComplete();
+    }
+
+    @Test
+    public void subscribeOnIO() throws InterruptedException {
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Path.of("text-file")))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(l -> {
+                    Assertions.assertFalse(l.isEmpty());
+                    log.info("Size {}", l.size());
+                    return true;
+                })
+                .verifyComplete();
+
+
     }
 }
